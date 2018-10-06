e4704bddb687a4bf406685ac78a89d79eab00b88
jbjw <github@jbjw.com>
2018-10-05 17:35:20 -0700

    auto-save

--- source/index.js
+++ source/index.js
@@ -2,6 +2,9 @@
 
 "use strict"
 
+import Game from "./Game.js"
+import Ship from "./Ship.js"
+
 const game = new Game()
 
 const ship1 = new Ship()
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
-const path = require( "path" )
+const path = require("path")
 
 module.exports = {
 	mode: "development",
 	entry: "./source/index.js",
 	output: {
 		filename: "bundle.js",
-		path: path.resolve( __dirname, "dist" )
-	}
+		path: path.resolve(__dirname, "dist"),
+	},
+	devServer: {
+		contentBase: path.join(__dirname, "dist"),
+		compress: true,
+		port: 80,
+		open: true,
+	},
 }
